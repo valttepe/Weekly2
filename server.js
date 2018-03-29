194d9b3690649a336fc47cabb650690a6578257f
valttepe <valtteri.pesu@hotmail.com>
2018-03-29 15:28:49 +0300

    project idea added to files

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
     },
 });
 
- const upload = multer({ storage });
+const upload = multer({storage});
 
 // resized images handling
 const sharp = require('sharp');
